63e06cc05a2d46d0b4de305ce2ddc9d9b0a11cf8
João Carlos Borges <joaocarlosifsc@hotmail.com>
2024-06-23 19:15:25 -0300

    melhorando a componentização dos tags e cards

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,17 +1,34 @@
 import Tag from '../Tag'
-import { Card, Descricao, Titulo } from './styles'
+import { Card, Descricao, Infos, Titulo } from './styles'
 
-const Product = () => (
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  description,
+  image,
+  infos,
+  system
+}: Props) => (
   <Card>
-    <img src="//placehold.it/222x250" alt="" />
-    <Titulo>Nome do jogo</Titulo>
-    <Tag>Categoria</Tag>
-    <Tag>Windows</Tag>
-    <Descricao>
-      Lorem iDescricaosum, dolor sit amet consectetur adipisicing elit. Itaque
-      ipsa amet quis ullam recusandae iusto! Earum, amet et? Ipsa vel possimus
-      rerum! FaciliDes placeat culpa obcaecati reiciendis iure, in accusantium!
-    </Descricao>
+    <img src={image} alt={title} />
+    <Infos>
+      {infos.map((infos) => (
+        <Tag key={infos}>{infos}</Tag>
+      ))}
+    </Infos>
+    <Titulo>{title}</Titulo>
+    <Tag>{category}</Tag>
+    <Tag>{system}</Tag>
+    <Descricao>{description}</Descricao>
   </Card>
 )
 
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,20 +1,48 @@
 import Product from '../Product'
 import { Container, List, Title } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <Title>{title}</Title>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
       </List>
     </div>
   </Container>
