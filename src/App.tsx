c67f8a2d11fe6738098bfb3f157b43b895ffeaed
João Carlos Borges <joaocarlosifsc@hotmail.com>
2024-06-25 20:54:34 -0300

    construindo as páginas e as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Header from './components/Header'
-import Banner from './components/Banner'
 import { GlobalCss } from './styles'
-import Product from './components/Product'
-import ProductList from './components/ProductsList'
-
-const rotas = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <Banner />
-        <ProductList title="Promoções" background="gray" />
-        <ProductList title="Em breve" background="black" />
-      </>
-    )
-  }
-])
+import Rotas from './routes'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalCss />
       <div className="container">
         <Header />
       </div>
-      <RouterProvider router={rotas} />
-    </>
+      <Rotas />
+    </BrowserRouter>
   )
 }
 
